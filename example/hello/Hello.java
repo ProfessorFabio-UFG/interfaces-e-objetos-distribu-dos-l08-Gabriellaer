ab2feaef7d2b0cb83515aa2690bfe99dee4fa28b
Gabriel Marques <116557711+Gabriellaer@users.noreply.github.com>
2025-05-17 09:59:19 -0300

    Update Hello.java

--- example/hello/Hello.java
+++ example/hello/Hello.java
@@ -43,5 +43,5 @@ import java.rmi.RemoteException;
 public interface Hello extends Remote {
     String sayHello() throws RemoteException;
     int soma(int a, int b) throws RemoteException;
-    String luckyNumber() throws RemoteException;
+    int luckyNumber() throws RemoteException;
 }
